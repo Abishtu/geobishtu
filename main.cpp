5ed437e027387b185a57ad4314857d16687d44aa
Harikesha Suresh <harikeshasuresh@gmail.com>
2024-09-07 10:45:55 +1000

    Got to a point where I know header is being read properly.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
-
 #include <iostream>
 #include <ShapeFile/ShapeFile.hpp>
 
-int main()
-{
-    std::cout << "Hello World!" << std::endl;
+using namespace std;
 
+int main()
+{    
+    ShapeFile shapeFile = ShapeFile("data/hotosm_aus_railways_lines_shp.shp");
+
+    cout << "File Code: " << shapeFile.getFileCode() << "    File Length: " << shapeFile.getLength(true) << endl;
+    cout << "Version: " << shapeFile.getVersion() << "    Shape Type: " << shapeFile.getShapeType() << endl;
+    
+    vector<double> bbMin = shapeFile.getBoundingBoxMin();
+
+    cout << "Bounding Box Min: " << endl << "\t(";
+
+    for (double bbPoint : bbMin) {
+        cout << bbPoint << ", ";
+    }
+
+    cout << ")" << endl;
+
+    vector<double> bbMax = shapeFile.getBoundingBoxMax();
+
+    cout << "Bounding Box Max: " << endl << "\t(";
+
+    for (double bbPoint : bbMax) {
+        cout << bbPoint << ", ";
+    }
+
+    cout << ")" << endl;
+    
     return 0;
 }
